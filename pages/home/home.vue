e7bc74db125ce5be919215ef011e096e646e3bab
“LXq-010605” <1084811235@qq.com>
2023-08-14 00:15:32 +0800

    'home'

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,21 @@
 <template>
-	<view>
-		home
-	</view>
+	<view>home</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+export default {
+	data() {
+		return {
+			// 这是轮播图的数据列表
+			swiperList: []
+		};
+	},
+	onLoad() {
+		// 调用方法，获取轮播图的数据
+		this.getSwiperList();
+	},
+	methods: {}
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
