98613f6e9f99a0b628374f6866374eaea3b782dc
“LXq-010605” <1084811235@qq.com>
2023-08-15 21:55:45 +0800

    合并冲突

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 import App from './App'
 import {
-  $http
+	$http
 } from '@escook/request-miniprogram'
 uni.$http = $http
 
 $http.baseUrl = 'https://api-hmugo-web.itheima.net'
+// 请求的根路径
 
 // 请求拦截器
 $http.beforeRequest = function(options) {
-  uni.showLoading({
-    title: '数据加载中....'
-  })
+	uni.showLoading({
+		title: '数据加载中....'
+	})
 }
 
 // 响应拦截器
 $http.afterRequest = function() {
-  uni.hideLoading()
+	uni.hideLoading()
 }
 
 // 封装弹框的方法
 uni.$showMsg = function(title = '数据请求失败!', duration = 1500) {
-  uni.showToast({
-    title,
-    duration,
-    icon: 'none'
-  });
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	});
 }
 
 // #ifndef VUE3
@@ -33,19 +34,19 @@ import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
 import {
-  createSSRApp
+	createSSRApp
 } from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
