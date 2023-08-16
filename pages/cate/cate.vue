64c08cae332f9fa539e26f38a01cfcc4048dc3ce
“LXq-010605” <1084811235@qq.com>
2023-08-16 23:02:44 +0800

    cate页第一次提交

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,22 +1,39 @@
 <template>
 	<view>
-		cate
+		<view class="scroll-view-container">
+			<scroll-view scroll-y="true" style="height: 300px">
+				<view></view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+				<view>xxxxx</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
