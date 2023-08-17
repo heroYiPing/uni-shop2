ea5ebcd54bcdf0457f699601ccea3e333e682ecb
liyi <1084811235@qq.com>
2023-08-17 17:05:10 +0800

    cate

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,39 +1,91 @@
 <template>
-	<view>
-		<view class="scroll-view-container">
-			<scroll-view scroll-y="true" style="height: 300px">
-				<view></view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-				<view>xxxxx</view>
-			</scroll-view>
-		</view>
-	</view>
+  <view>
+    <view class="scroll-view-container">
+      <scroll-view class="left-scroll-view" :style="{ height: wh + 'px' }" scroll-y="true">
+        <view></view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+      </scroll-view>
+      <!-- 右侧滑动区域 -->
+      <scroll-view scroll-y="true" :style="{ height: wh + 'px' }" style="height: 300px">
+        <view></view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+        <view>xxxxx</view>
+      </scroll-view>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
-	data() {
-		return {};
-	},
-	methods: {}
+  data() {
+    return {
+      wh: 0
+    };
+  },
+  methods: {},
+  onLoad() {
+    const sysInfo = uni.getWindowInfo();
+    console.log(sysInfo);
+    this.wh = sysInfo.windowHeight;
+    console.log(this.wh, 'wh');
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.scroll-view-container {
+  display: flex;
+  .left-scroll-view {
+    width: 120px;
+  }
+}
+</style>
