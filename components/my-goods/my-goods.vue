b75fe0ac25ab4b7967464a2f338f2181cfc030e5
heroYiPing <1084811235@qq.com>
2023-11-26 22:18:40 +0800

    完成goods_list页码的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="goods_item">
+		<view class="goods_item_left">
+			<img :src="goods.goods_small_logo || defaultImage" alt="" class="goods_pic" />
+		</view>
+		<view class="goods_item_right">
+			<!-- 商品名称 -->
+			<view class="goods_name">
+				{{ goods.goods_name }}
+			</view>
+			<!-- 商品价格 -->
+			<view class="goods_price">￥{{ goods.goods_price | tofixed }}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'my-goods',
+	props: {
+		goods: {
+			type: Object,
+			default: {}
+		}
+	},
+	data() {
+		return {
+			defaultImage: 'http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000193373779_1_400x400.jpg'
+		};
+	},
+	filters: {
+		// 把数字处理为带两位小数点的数字
+		tofixed(num) {
+			return Number(num).toFixed(2);
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.goods_item {
+	display: flex;
+	flex-wrap: nowrap;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.goods_item_left {
+		margin-right: 5px;
+		.goods_pic {
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.goods_item_right {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods_name {
+			font-size: 13px;
+		}
+		.goods_price {
+			font-size: 16px;
+			color: #c00;
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,24 +1,10 @@
 <template>
 	<view>
 		<view class="goods_list">
-			<block v-for="item in goodsList" :key="item.goods_id">
+			<view v-for="item in goodsList" :key="item.goods_id" @click="goToDetail(item)">
 				<!-- 每一项商品 -->
-				<view class="goods_item">
-					<view class="goods_item_left">
-						<img :src="item.goods_small_logo" alt="" class="good_pic" />
-					</view>
-					<view class="goods_item_right">
-						<!-- 商品名称 -->
-						<view class="goods_name">
-							{{ item.goods_name }}
-						</view>
-						<!-- 商品价格 -->
-						<view class="goods_price">
-							{{ item.goods_price }}
-						</view>
-					</view>
-				</view>
-			</block>
+				<my-goods :goods="item"></my-goods>
+			</view>
 		</view>
 	</view>
 </template>
@@ -35,7 +21,9 @@ export default {
 				pagesize: 10
 			},
 			goodsList: [],
-			total: 0
+			total: 0,
+			defaultImage: 'http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000193373779_1_400x400.jpg',
+			isLoading: false
 		};
 	},
 	onLoad(options) {
@@ -46,15 +34,73 @@ export default {
 	},
 	methods: {
 		// 获取商品列表数据
-		async getGoodsList() {
+		async getGoodsList(cb) {
+			// 打开节流阀
+			this.isLoading = true;
 			const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj);
+			// 关闭节流阀
+			this.isLoading = false;
+			cb && cb();
 			if (res.meta.status !== 200) return uni.$showMsg();
-			this.queryObj.total = res.message.total;
-			this.goodsList = res.message.goods;
+			this.total = res.message.total;
+			// this.goodsList = res.message.goods;
+			this.goodsList = [...this.goodsList, ...res.message.goods];
 			console.log(res, 'res');
+		},
+
+		goToDetail(goods) {
+			uni.navigateTo({
+				url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+			});
 		}
+	},
+	onReachBottom() {
+		// 判断是否还有下一页
+		if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕!');
+		// 如果正在请求数据 则直接跳出方法
+		if (this.isLoading) return;
+		// 让页码值自增+1
+		this.queryObj.pagenum += 1;
+		this.getGoodsList();
+	},
+	onPullDownRefresh() {
+		// 重置关键数据
+		this.queryObj.pagenum = 1;
+		this.total = 0;
+		this.isLoading = false;
+		this.goodsList = [];
+
+		// 重新发起数据请求
+		this.getGoodsList(() => uni.stopPullDownRefresh());
 	}
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.goods_item {
+	display: flex;
+	flex-wrap: nowrap;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.goods_item_left {
+		margin-right: 5px;
+		.goods_pic {
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.goods_item_right {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods_name {
+			font-size: 13px;
+		}
+		.goods_price {
+			font-size: 16px;
+			color: #c00;
+		}
+	}
+}
+</style>
