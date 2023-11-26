a91ab6ff1ed4379fb8b426346b296b7fd0d85708
heroYiPing <1084811235@qq.com>
2023-11-26 23:35:17 +0800

    goodsdetail 第一次提交

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -5,7 +5,21 @@
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			goods_info: {}
+		};
+	},
+	methods: {
+		async getGoodsDetail(goods_id) {
+			const { data: res } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id });
+			if (res.meta.status !== 200) return uni.$showMsg();
+			this.goods_info = res.message;
+		}
+	},
+	onLoad(options) {
+		console.log(options);
+		const goods_id = options.goods_id;
+		this.getGoodsDetail(goods_id);
 	}
 };
 </script>
