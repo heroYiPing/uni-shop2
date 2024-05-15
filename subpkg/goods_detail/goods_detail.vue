542240ab3c378c44ca6b31413e17c268872b9be8
heroYiPing <1084811235@qq.com>
2024-05-15 23:08:39 +0800

    商品详情

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,5 +1,5 @@
 <template>
-	<view>
+	<view v-if="goods_info.goods_name">
 		<!-- 轮播区域 -->
 		<swiper :indicator-dots="true" class="swp" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
 			<swiper-item v-for="(item, i) in goods_info.pics" :key="i">
@@ -10,17 +10,21 @@
 		<!-- 商品信息区域 -->
 		<view class="goods-info-box">
 			<!-- 商品价格 -->
-			<view class="goods_price">
-				{{ goods_info.goods_price }}
-			</view>
+			<view class="goods_price">￥{{ goods_info.goods_price }}</view>
 			<!-- 商品信息主体区域 -->
 			<view class="goods-info-body">
 				<!-- 商品名字 -->
-				<view class="goods-name"></view>
+				<view class="goods-name">{{ goods_info.goods_name }}</view>
 				<!-- 收藏 -->
+				<view class="favi">
+					<uni-icons type="star" size="30" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
 			</view>
 			<!-- 商品运费 -->
+			<view class="yf">快递：免运费</view>
 		</view>
+		<rich-text :nodes="goods_info.goods_introduce"></rich-text>
 	</view>
 </template>
 
@@ -35,6 +39,7 @@ export default {
 		async getGoodsDetail(goods_id) {
 			const { data: res } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id });
 			if (res.meta.status !== 200) return uni.$showMsg();
+			res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:bolck";').replace(/webp/g, 'jpg');
 			this.goods_info = res.message;
 		},
 		// 进行预览
@@ -62,6 +67,38 @@ export default {
 	.img {
 		width: 100%;
 		height: 100%;
+	}
+}
+.goods-info-box {
+	padding: 10px;
+	padding-right: 0px;
+	.goods_price {
+		color: #c00000;
+		font-size: 18px;
+		margin: 10px 0;
+	}
+	.goods-info-body {
+		display: flex;
+		justify-content: space-between;
+		.goods-name {
+			font-size: 13px;
+			margin-right: 10px;
+		}
+		.favi {
+			width: 120px;
+			font-size: 12px;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			border-left: 1px solid #efefef;
+			color: gray;
+		}
+	}
+	.yf {
+		font-size: 12px;
+		color: gray;
+		margin: 10px 0;
 	}
 }
 </style>
