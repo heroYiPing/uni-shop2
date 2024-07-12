2143fcb2794966936c6e539e269229be9064a831
liyi <1084811235@qq.com>
2024-07-12 17:09:22 +0800

    代码提交

--- mixins/tabbar-badge.js
+++ mixins/tabbar-badge.js
@@ -1,20 +1,20 @@
 import {
-	mapGetters
+  mapGetters
 } from 'vuex';
 
 export default {
-	methods: {
-		setBadge() {
-			uni.setTabBarBadge({
-				index: 2,
-				text: this.total + ''
-			});
-		}
-	},
-	onShow() {
-		this.setBadge();
-	},
-	computed: {
-		...mapGetters('m_cart', ['total'])
-	}
+  methods: {
+    setBadge() {
+      uni.setTabBarBadge({
+        index: 2,
+        text: this.total == 0 ? '' : this.total + ''
+      });
+    }
+  },
+  onShow() {
+    this.setBadge();
+  },
+  computed: {
+    ...mapGetters('m_cart', ['total'])
+  }
 }
