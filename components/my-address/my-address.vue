a0a9936ad622966eae12b2137cf2f9a2c76e6ce5
heroYiPing <1084811235@qq.com>
2024-08-07 23:37:38 +0800

    选择收货地址

--- components/my-address/my-address.vue
+++ components/my-address/my-address.vue
@@ -1,11 +1,32 @@
 <template>
 	<view>
 		<!-- 收货地址组件 -->
-		<view class="address-choose-box">
+		<view class="address-choose-box" v-if="JSON.stringify(address) === '{}'">
 			<button type="primary" size="mini">请选择收货地址+</button>
 		</view>
 
 		<!-- 渲染收货信息的盒子 -->
+		<view class="address-info-box" v-else>
+			<view class="row1">
+				<view class="row1-left">
+					<view class="username">
+						收货人：
+						<text>escook</text>
+					</view>
+				</view>
+				<view class="row1-right">
+					<view class="phone">
+						电话：
+						<text>138XXXX5555</text>
+					</view>
+					<uni-icons type="arrowright" size="16"></uni-icons>
+				</view>
+			</view>
+			<view class="row2">
+				<view class="row2-left">收货地址：</view>
+				<view class="row2-right">河北省邯郸市肥乡区xxx 河北省邯郸市肥乡区xxx 河北省邯郸市肥乡区xxx 河北省邯郸市肥乡区xxx</view>
+			</view>
+		</view>
 
 		<!-- 底部的边框线 -->
 		<image src="/static/cart_border@2x.png" class="address-border"></image>
@@ -16,7 +37,11 @@
 export default {
 	name: 'my-address',
 	data() {
-		return {};
+		return {
+			address: {
+				username: 'zs'
+			}
+		};
 	}
 };
 </script>
@@ -32,5 +57,41 @@ export default {
 	display: block;
 	width: 100%;
 	height: 5px;
+}
+
+// 渲染收货信息的盒子
+.address-info-box {
+	font-size: 12px;
+	height: 90px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 0 5px;
+
+	// 第一行
+	.row1 {
+		display: flex;
+		justify-content: space-between;
+
+		.row1-right {
+			display: flex;
+			align-items: center;
+
+			.phone {
+				margin-right: 5px;
+			}
+		}
+	}
+
+	// 第二行
+	.row2 {
+		display: flex;
+		align-items: center;
+		margin-top: 10px;
+
+		.row2-left {
+			white-space: nowrap;
+		}
+	}
 }
 </style>
